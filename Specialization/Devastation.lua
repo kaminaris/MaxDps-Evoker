d446befa25153d5fa3e5fd88559b7a5a824162d9
Pawel <wilkowski@gmail.com>
2022-10-31 19:47:33 +0100

    initial

---
+++ Specialization/Devastation.lua
@@ -0,0 +1,26 @@
+local _, addonTable = ...;
+
+--- @type MaxDps
+if not MaxDps then return end;
+
+local Evoker = addonTable.Evoker;
+local MaxDps = MaxDps;
+
+local HL = {
+
+};
+
+local CN = {
+	None      = 0,
+	Kyrian    = 1,
+	Venthyr   = 2,
+	NightFae  = 3,
+	Necrolord = 4
+};
+
+setmetatable(HL, Evoker.spellMeta);
+
+function Evoker:Devastation()
+    local fd = MaxDps.FrameData;
+
+end
